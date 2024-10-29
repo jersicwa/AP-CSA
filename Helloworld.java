17bbe8a1564fce51a3725cf0556a70182c462627
jersicwa <jessboll16@gmail.com>
2024-10-29 15:53:11 -0500

    Create Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,26 @@
+/*
+Jessica Boll
+10/29/24
+Writing Hello World
+*/
+
+/*
+Class Declaration: identifies the name, the start, and the end of the class.
+The class name MUST match the file name. (i.e. 1_1 in this case)
+*/
+public class Helloworld {
+    /*
+    main Method: controls all of the action in the program
+    */
+    public static void main(String[] args) {
+        System.out.print("AP ");
+        /* 
+        System.out: object that generates output in the console
+            System.out.print -- displays whatever is in the () on the screen
+            System.out.println -- display whatever is in the () on the screen, then move the cursor to the next line
+        */
+        System.out.print("CS ");
+        System.out.print("Rocks!");
+    }
+
+}
