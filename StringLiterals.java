e9ad6d6acd11594d0dc6c1798bf03dc4e144e73e
jersicwa <jessboll16@gmail.com>
2024-11-03 11:50:41 -0600

    meow

--- StringLiterals.java
+++ StringLiterals.java
@@ -5,6 +5,10 @@ public class StringLiterals {
          * an exact sequence of characters (letters, numbers, or symbols) which are enclosed between two quotation marks
          * ex: "This is a string literal"
          */
+        System.out.println("This is a string literal");
+        System.out.println("ABC123$#%abc");
+        System.out.println("Have a great day!!");
+        System.out.println("AP CS A ROCKS!!");
     }
     
 }
