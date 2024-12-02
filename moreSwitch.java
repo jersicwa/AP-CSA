012d8331e28ce6ba316daff851889bdad335b861
jersicwa <jessboll16@gmail.com>
2024-12-01 22:41:56 -0600

    painful ahh

--- moreSwitch.java
+++ moreSwitch.java
@@ -1,16 +1,27 @@
+import java.util.Scanner;
+
 public class moreSwitch {
     public static void main(String[] args) {
-        char selection = 'C';
-        switch (selection) {
-            case 'A':
-                System.out.println("A");
-                break;
-            case 'B':
-            System.out.println("B");
-            break;
-            case 'C':
-                System.out.println("C");
-            break;
-        }
+        Scanner input=new Scanner(System.in);
+        System.out.println("How much does your package weigh?");
+        double weight = input.nextDouble();
+        System.out.println("Would you like express or regular shipping?");
+        String type = input.nextLine();
+        double costReg = 0;
+
+        if (type.equals("regular") || type.equals("Regular"))
+            if (weight<=2)
+                costReg=1.1;
+            else if (weight >2 && weight <6)
+                costReg=2.2;
+            else if (weight>6 && weight<10)
+                costReg = 3.7;
+            else if (weight >10)
+                costReg = 4.5;
+        if (type.equals("express")||type.equals("Express"))
+            costReg = costReg+3;
+
+        System.out.println("Your package costs "+costReg);
+
     }
 }
