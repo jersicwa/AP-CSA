16697a07a8e5790d64c56dc21f86b2d502871e35
jersicwa <jessboll16@gmail.com>
2024-12-16 21:46:04 -0600

    painful ahh

---
+++ arrayPrac.java
@@ -0,0 +1,37 @@
+public class arrayPrac {
+    public static void main(String[] args) {
+        // if you know the value of the array
+        //int[] temps={54, 60, 62, 58, 65, 68, 69};
+       // int length = temps.length;
+        //String s="ab";
+        //s.length();
+        // if you don't know the value of the array
+        // you have to know how many values are in your array
+        //int [] temps2=new int[7]; // new" allocate memory for this array
+        //default value of int is 0
+        //update values, access each by index
+       /* temps2[0]=54;
+        temps2[1]=60;
+        temps2[2]=62;
+        temps2[3]=58;
+        temps2[4]=65;
+        temps2[5]=68;
+        temps2[6]=69;
+        //if you put in temps2[7] it won't work since you don't have memory for more than 7 integers
+        for(int i=0;i< temps2.length;i++)
+        {
+            System.out.println(temps2[i]);
+            //total+=temps2[]
+        }
+
+
+        String[] places = {"Atlanta", "NewYork", "Roswell", "Johns Creek"};
+        for(int i=0;i<places.length;i++){
+            System.out.println(places[i]);
+
+        */
+        //String[] names={"Malfoy", "Crabbe", "Goyle", "Pansy", "Dean"};
+
+        //}
+    }
+}
