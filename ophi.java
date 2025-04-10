5aac27557f28300fd84b626c0fc4d87a271aaadc
jersicwa <jessboll16@gmail.com>
2025-04-10 12:20:06 -0500

    painful ahh

---
+++ ophi.java
@@ -0,0 +1,9 @@
+public class ophi {
+    ArrayList<Integer> numList = new ArrayList<Integer>();
+        numList.add(3);
+        numList.add(2);
+        numList.add(1);
+        numList.add(1, 0);
+        numList.set(0, 2);
+        System.out.print(numList);
+}
